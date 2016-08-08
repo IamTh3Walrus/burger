540e7ea6859739b37ac67f4a56499f4edffb8a03
Stephen Doe <redcard1016@yahoo.com>
2016-08-08 18:15:34 -0400

    Finished overall structure

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,9 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers 
-(
-id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(255) NOT NULL,
-devoured BOOLEAN NOT NULL,
-date TIMESTAMP NOT NULL,
-PRIMARY KEY (id)
-);
+CREATE TABLE burgers(
+id int AUTO_INCREMENT,
+PRIMARY KEY (id),
+burger_name varchar(100) NOT NULL,
+devoured BOOLEAN default false,
+date TIMESTAMP);
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO Burgers (burger_name)
+VALUES ('Royal');
+INSERT INTO Burgers (burger_name)
+VALUES ('Royal with Cheese');
+INSERT INTO Burgers (burger_name)
+VALUES ('Blue Cheese Bacon Burger');
+INSERT INTO Burgers (burger_name)
+VALUES ('Barbecue Cheddar Burger');
+	
